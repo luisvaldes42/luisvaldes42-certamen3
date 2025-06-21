39611b98334558afc0981534afa8023372d80117
Luih <luis.valdes1@virginiogomez.cl>
2025-06-20 21:21:57 -0400

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,13 +2,13 @@ temperaturas = input("Ingresa aquí 5 grados de temperatura distintos, separados
 numeros = [float(n.strip()) for n in temperaturas.split(",")]
 
 if all(15 <= n <= 30 for n in numeros):
-    print("Todas las temperaturas están dentro del rango de 15 a 30 grados.")
+    print("Todas las temperaturas estan dentro del rango de 15 a 30 grados.")
 else:
-    print("No todas las temperaturas están dentro del rango de 15 a 30 grados.")
+    print("No todas las temperaturas estan dentro del rango de 15 a 30 grados.")
 
 for n in numeros:
     if n < 15 or n > 30:
-        print(f"La temperatura {n} está fuera del rango de 15 a 30 grados.")
+        print(f"La temperatura {n} esta fuera del rango de 15 a 30 grados.")
 
 promedio = sum(numeros) / len(numeros)
 print("Su promedio es: ", promedio)
